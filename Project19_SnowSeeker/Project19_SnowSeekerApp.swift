0a49b0274b8a2e501eea2822407ad7d67709b6dc
Matvii Artemenko <100190098+MatveyArtemenko@users.noreply.github.com>
2023-03-18 11:06:11 +0000

    Initial Commit

---
+++ Project19_SnowSeeker/Project19_SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project19_SnowSeekerApp.swift
+//  Project19_SnowSeeker
+//
+//  Created by admin on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Project19_SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
